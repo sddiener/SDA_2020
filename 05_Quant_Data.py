864fff88c5adbf69c382b6da98b4e57e9bb614c5
sddiener <55442757+sddiener@users.noreply.github.com>
2020-12-22 18:40:28 +0100

    Added models

--- 05_Quant_Data.py
+++ 05_Quant_Data.py
@@ -1,3 +1,5 @@
+import pickle
+
 import pandas as pd
 import numpy as np
 from matplotlib import pyplot as plt
@@ -6,68 +8,39 @@ import datetime
 import seaborn as sns
 import sklearn
 import urllib.request
-from sklearn.linear_model import ElasticNetCV
-from sklearn.metrics import mean_squared_error
-from sklearn.model_selection import train_test_split
+
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.linear_model import ElasticNetCV, ElasticNet, LinearRegression
+from sklearn.metrics import mean_squared_error, accuracy_score, r2_score
+from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.preprocessing import StandardScaler
 
 # %% Download macro data -------------------------------------------------------------------------------------
 
 # import data from fred
-start = datetime.datetime(1999, 9, 1) #sentiment indicator starts from 2000
+start = datetime.datetime(1999, 9, 1)  # sentiment indicator starts from 2000
 end = datetime.date.today()
 
-GDP_data = web.DataReader('CPMNACSAB1GQCH', 'fred', start, end).to_period('Q') # Gross Domestic Product for Switzerland (CPMNACSAB1GQCH)	
-Unemp_data = web.DataReader('LMUNRRTTCHQ156N', 'fred', start, end).to_period('Q') # Registered Unemployment Rate for Switzerland (LMUNRRTTCHQ156N)
-rec_indicator = web.DataReader('CHEREC', 'fred', start, end).to_period('Q') # OECD based Recession Indicators for Switzerland from the Period following the Peak through the Trough (CHEREC)
-Import_data = web.DataReader('CHEIMPORTQDSMEI', 'fred', start, end).to_period('Q') # Imports of Goods and Services in Switzerland (CHEIMPORTQDSMEI)
-Export_data = web.DataReader('CHEEXPORTQDSMEI', 'fred', start, end).to_period('Q') # Exports of Goods and Services in Switzerland (CHEEXPORTQDSMEI)
-GovSpend_data = web.DataReader('CHEGFCEQDSMEI', 'fred', start, end).to_period('Q') # Government Final Consumption Expenditure in Switzerland (CHEGFCEQDSMEI)	
-Consumption_data = web.DataReader('CHEPFCEQDSMEI', 'fred', start, end).to_period('Q') #Private Final Consumption Expenditure in Switzerland (CHEPFCEQDSMEI)	
+GDP_data = web.DataReader('CPMNACSAB1GQCH', 'fred', start, end).to_period(
+    'Q')  # Gross Domestic Product for Switzerland (CPMNACSAB1GQCH)
+Unemp_data = web.DataReader('LMUNRRTTCHQ156N', 'fred', start, end).to_period(
+    'Q')  # Registered Unemployment Rate for Switzerland (LMUNRRTTCHQ156N)
+rec_indicator = web.DataReader('CHEREC', 'fred', start, end).to_period(
+    'Q')  # OECD based Recession Indicators for Switzerland from the Period following the Peak through the Trough (
+# CHEREC)
+Import_data = web.DataReader('CHEIMPORTQDSMEI', 'fred', start, end).to_period(
+    'Q')  # Imports of Goods and Services in Switzerland (CHEIMPORTQDSMEI)
+Export_data = web.DataReader('CHEEXPORTQDSMEI', 'fred', start, end).to_period(
+    'Q')  # Exports of Goods and Services in Switzerland (CHEEXPORTQDSMEI)
+GovSpend_data = web.DataReader('CHEGFCEQDSMEI', 'fred', start, end).to_period(
+    'Q')  # Government Final Consumption Expenditure in Switzerland (CHEGFCEQDSMEI)
+Consumption_data = web.DataReader('CHEPFCEQDSMEI', 'fred', start, end).to_period(
+    'Q')  # Private Final Consumption Expenditure in Switzerland (CHEPFCEQDSMEI)
 
 # import the sentiment indicator
-NetSentiment_data = pd.read_csv('data/NetSentiment').rename(columns={'date':'DATE', '0':'Sentiment'})
-NetSentiment_data['DATE'] = pd.to_datetime(NetSentiment_data['DATE']).dt.to_period('Q') #transform date into quarter
+NetSentiment_data = pd.read_csv('data/NetSentiment').rename(columns={'date': 'DATE', '0': 'Sentiment'})
+NetSentiment_data['DATE'] = pd.to_datetime(NetSentiment_data['DATE']).dt.to_period('Q')  # transform date into quarter
 NetSentiment_data = NetSentiment_data.groupby('DATE').sum()
-
-
-# GDP_url = "https://sdw.ecb.europa.eu/quickviewexport.do;jsessionid=44DBD7559D119BEF9DA13CCEB70C23E1?SERIES_KEY=MNA.Q.Y.I8.W2.S1.S1.B.B1GQ._Z._Z._Z.EUR.V.N&type=csv"
-# Eurlibor_url = "https://sdw.ecb.europa.eu/quickviewexport.do;jsessionid=0014F48DEAEA2500294F6E3962E49775?SERIES_KEY=143.FM.M.U2.EUR.RT.MM.EURIBOR1YD_.HSTA&type=csv"
-# Consumption_url = "https://sdw.ecb.europa.eu/quickviewexport.do;jsessionid=1573D4C068E9F1CA1116D3998B81380E?SERIES_KEY=MNA.Q.Y.I8.W0.S1M.S1.D.P31._Z._Z._T.EUR.V.N&type=csv"
-# GovSpend_url = "https://sdw.ecb.europa.eu/quickviewexport.do;jsessionid=3F35BD60FBC44B0E9380EFA27A0622CF?SERIES_KEY=MNA.Q.Y.I8.W0.S13.S1.D.P3._Z._Z._T.EUR.V.N&type=csv"
-# Export_url = "https://sdw.ecb.europa.eu/quickviewexport.do;jsessionid=5A4DE9E92D17B294D6A5315EE8355389?SERIES_KEY=MNA.Q.Y.I8.W1.S1.S1.D.P6._Z._Z._Z.EUR.V.N&type=csv"
-# Import_url = "https://sdw.ecb.europa.eu/quickviewexport.do;jsessionid=E521FE1B420F2DADB8A4B3B88B00B6A9?SERIES_KEY=MNA.Q.Y.I8.W1.S1.S1.C.P7._Z._Z._Z.EUR.V.N&type=csv"
-# Unemp_url = "https://sdw.ecb.europa.eu/quickviewexport.do;jsessionid=B3312C4AA394270B0016D1C9BFD78ECB?SERIES_KEY=ENA.Q.Y.I8.W2.S1.S1._Z.EMP._Z._T._Z.PS._Z.N&type=csv"
-
-# # GDP typically consisty of Y = C + I + G + (X-M)
-# urllib.request.urlretrieve(GDP_url, "data/GDP_data.csv")
-# urllib.request.urlretrieve(Eurlibor_url, "data/Eurlibor_data.csv")
-# urllib.request.urlretrieve(Consumption_url, "data/Consumption_data.csv")
-# urllib.request.urlretrieve(GovSpend_url, "data/GovSpend_data.csv")
-# urllib.request.urlretrieve(Export_url, "data/Export_data.csv")
-# urllib.request.urlretrieve(Import_url, "data/Import_data.csv")
-# urllib.request.urlretrieve(Unemp_url, "data/Unemp_data.csv")
-
-# GDP_data = pd.read_csv('data/GDP_data.csv', skiprows=range(1, 5))
-# Eurlibor_data = pd.read_csv('data/Eurlibor_data.csv', skiprows=range(1, 5))
-# Consumption_data = pd.read_csv('data/Consumption_data.csv', skiprows=range(1, 5))
-# GovSpend_data = pd.read_csv('data/GovSpend_data.csv', skiprows=range(1, 5))
-# Export_data = pd.read_csv('data/Export_data.csv', skiprows=range(1, 5))
-# Import_data = pd.read_csv('data/Import_data.csv', skiprows=range(1, 5))
-# Unemp_data = pd.read_csv('data/Unemp_data.csv', skiprows=range(1, 5))
-
-
-
-# %% Data Cleaning
-
-# Convert absolutes to change over period
-# GDP_data = GDP_data.multiply(10 ^ 6).pct_change(-1)
-# Consumption_data = Consumption_data.multiply(10 ^ 6).pct_change(-1)
-# GovSpend_data = GovSpend_data.multiply(10 ^ 6).pct_change(-1)
-# Export_data = Export_data.multiply(10 ^ 6).pct_change(-1)
-# Import_data = Import_data.multiply(10 ^ 6).pct_change(-1)
-# Unemp_data = Unemp_data.multiply(10 ^ 6).pct_change(-1)
-
 
 GDP_data = np.log(GDP_data).diff()
 Consumption_data = np.log(Consumption_data).diff()
@@ -76,12 +49,11 @@ Export_data = np.log(Export_data).diff()
 Import_data = np.log(Import_data).diff()
 Unemp_data = np.log(Unemp_data).diff()
 
-
 # Construct final DataFrame
 data = [GDP_data, Consumption_data, GovSpend_data, Export_data, Import_data, Unemp_data, NetSentiment_data]
 data = pd.concat(data, axis=1)
 data.columns = ["GDP", "Consumption", "GovSpend", "Exports", "Imports", "Unemp", 'NetSentiment']
-data.index = data.index.to_timestamp() 
+data.index = data.index.to_timestamp()
 
 # remove NA caused by growth rate calculation
 data = data.dropna()
@@ -110,36 +82,70 @@ fig.set_size_inches(12, 8)
 
 fig.savefig('plots/economic_varibales.png', dpi=300)
 
-
 # %% Model Prep
 
 # split data into X / Y
-#Y = data['GDP_gr']
 Y = data['GDP']
 X = data.loc[:, data.columns != 'GDP']
-#X = StandardScaler().fit_transform(X)  # Standardize
 
 # Split data into train / test
 X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.3, random_state=2020)
 
 # %% Model Train
 
-alphas = [0.0001, 0.001, 0.01, 0.1, 0.3, 0.5, 0.7, 1]
-elastic_cv = ElasticNetCV(alphas=alphas, cv=5)
+# specify models and parameters
+model_OLS = LinearRegression()
+param_OLS = {'fit_intercept': [True, False],
+             'normalize': [True, False]}
 
-model = elastic_cv.fit(X_train, y_train)
-y_pred = model.predict(X_test)
-score = model.score(X_test, y_test)
-mse = mean_squared_error(y_test, y_pred)
+grid_OLS = GridSearchCV(model_OLS, param_OLS, cv=5, verbose=0, n_jobs=-1, return_train_score=True)
+grid_OLS = grid_OLS.fit(X_train, y_train)
+y_pred_OLS = grid_OLS.predict(X_test)
 
-print("R2:{0:.4f}, MSE:{1:.4f}, RMSE:{2:.4f}".format(score, mse, np.sqrt(mse)))
+model_ELN = ElasticNet()
+param_ELN = {'alpha': [0.0001, 0.001, 0.01, 0.1, 0.3, 0.5, 0.7, 1]}
+
+grid_ELN = GridSearchCV(model_ELN, param_ELN, cv=5, verbose=0, n_jobs=-1, return_train_score=True)
+grid_ELN = grid_ELN.fit(X_train, y_train)
+y_pred_ELN = grid_ELN.predict(X_test)
+
+model_RF = RandomForestRegressor()
+param_RF = {
+    'bootstrap': [True, False],
+    'max_depth': [10, 50, 100, 500, 1000],
+    'min_samples_split': [2, 5, 10],
+    'n_estimators': [10, 100, 500]
+}
+
+grid_RF = GridSearchCV(model_RF, param_RF, cv=5, verbose=0, n_jobs=-1, return_train_score=True)
+grid_RF = grid_RF.fit(X_train, y_train)
+y_pred_RF = grid_RF.predict(X_test)
+
+# group
+models = [model_OLS, model_ELN, model_RF]
+parameters = [param_OLS, param_ELN, param_RF]
+grids = [grid_OLS, grid_ELN, grid_RF]
+y_preds = [y_pred_OLS, y_pred_ELN, y_pred_RF]
+
+# calculate scores
+for model, param, grid, y_pred in zip(models, parameters, grids, y_preds):
+    name = type(model).__name__
+    print("Tuning Hyperparameters: {}".format(name))
+
+    #print("Score: ", grid.score(y_test, y_pred))
+    print("MSE: ", mean_squared_error(y_test, y_pred))
+    print("R2: ", r2_score(y_test, y_pred))
+
+    with open("models/gridsearch_{}.pkl".format(name), 'wb') as file:
+        pickle.dump(grid, file)
 
 # %% Plot results
-plt.figure(figsize=(30,10))
-
+plt.figure(figsize=(30, 10))
 x_ax = range(len(X_test))
-plt.scatter(x_ax, y_test, s=5, color='blue', label='original')
-plt.plot(x_ax, y_pred, lw=0.8, color='red', label='predicted')
+plt.scatter(x_ax, y_test, s=5, color='black', label='original')
+plt.plot(x_ax, y_pred_OLS, lw=0.8, color='blue', label='OLS')
+plt.plot(x_ax, y_pred_ELN, lw=0.8, color='green', label='ElasticNet')
+plt.plot(x_ax, y_pred_RF, lw=0.8, color='orange', label='RandomForest')
 plt.legend()
-plt.show()
+#plt.show()
 plt.savefig('plots/model_results.png')
